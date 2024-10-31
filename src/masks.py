bb515b1bc4d18845766480f2b6baa11af4b02269
Дмитрий Коваль <bloodpaperace@mail.ru>
2024-10-31 15:35:31 +0300

    внесены правки в функций по маскеровке данных модуля masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,12 +1,8 @@
 def get_mask_card_number(card_number: str) -> str:
     """Функция получает на вход номер карты и возвращает его в формате XXXX XX** **** XXXX"""
-    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+    return f"{card_number[:-17]} {card_number[-16:-12]} {card_number[-12:-10]}** **** {card_number[-4:]}"
 
 
 def get_mask_account(bank_account_number: str) -> str:
     """Функция получает на вход номер счета и возвращает его в формате **XXXX"""
-    return f"**{bank_account_number[-4:]}"
-
-
-print(get_mask_card_number("7000792289606361"))
-print(get_mask_account("73654108430135874305"))
+    return f"{bank_account_number[:-21]} **{bank_account_number[-4:]}"
